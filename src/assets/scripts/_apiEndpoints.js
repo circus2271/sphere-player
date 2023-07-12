0aa603e4c959a9eda80a7ee46bd0da4171159226
circus2271 <rodionov134@gmail.com>
2023-07-12 09:13:45 +0300

    get playlist names; show them

--- src/assets/scripts/_apiEndpoints.js
+++ src/assets/scripts/_apiEndpoints.js
@@ -1,4 +1,4 @@
-const basePath = ' https://europe-central2-sphere-385104.cloudfunctions.net/'
+const basePath = 'https://europe-central2-sphere-385104.cloudfunctions.net'
 
 export const loginApiEndpoint = `${basePath}/login`
 export const getRecordsApiEndpoint = `${basePath}/getRecords`
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,4 +1,5 @@
 import { handleLogin } from './handleLogin'
+import { getRecordsApiEndpoint } from './_apiEndpoints'
 
 const getBaseId = async () => {
   const baseId = await new Promise(resolve => {
@@ -10,4 +11,15 @@ const getBaseId = async () => {
 
 (async () => {
   const baseId = await getBaseId()
+  const queryParams = { baseId, tableId: 'Info'}
+  const searchParams = new URLSearchParams(queryParams)
+  const urlToFetchRecords = `${getRecordsApiEndpoint}?${searchParams}`
+  const response = await fetch(urlToFetchRecords)
+  const records = await response.json()
+  
+  console.log(records)
+  records.forEach(record => {
+    const playlistName = record.fields['Name']
+    alert(playlistName)
+  })
 })()
