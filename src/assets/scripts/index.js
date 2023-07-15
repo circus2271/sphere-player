46187f2e4a4bbe8cf6a60f9b8582e5b91f46efa8
circus2271 <rodionov134@gmail.com>
2023-07-15 06:41:01 +0300

    add styles; change markup; improve script; add fonts

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -5,46 +5,10 @@ const getBaseId = async () => {
   const baseId = await new Promise(resolve => {
     handleLogin(resolve)
   })
-  
+
   return baseId
 }
 
-(async () => {
-  const baseId = await getBaseId()
-  const queryParams = { baseId, tableId: 'Info' }
-  const searchParams = new URLSearchParams(queryParams)
-  const urlToFetchRecords = `${getRecordsApiEndpoint}?${searchParams}`
-  const response = await fetch(urlToFetchRecords)
-  const records = await response.json()
-  
-  console.log(records)
-  
-  const playlists = document.querySelector('#playlists')
-  
-  playlists.textContent = ''
-  records.forEach((record, i) => {
-    const playlistName = record.fields['Name']
-    const playlistDescription = record.fields['Notes']
-    const selected = i === 0
-    
-    if (selected) {
-      document.querySelector('#current-playlist').innerHTML = playlistName
-    }
-    
-    const html = `
-      <button class="playlist ${selected ? 'playlist--selected' : ''}">
-        <span class="playlist__name">
-          ${playlistName}
-        </span>
-        <span class="playlist__description">
-          ${playlistDescription}
-        </span>
-      </button>
-    `
-
-    playlists.innerHTML += html
-  })
-})()
 
 // const records = [
 //   {
@@ -66,3 +30,41 @@ const getBaseId = async () => {
 //     }
 //   }
 // ]
+
+(async () => {
+  const baseId = await getBaseId()
+  const queryParams = { baseId, tableId: 'Info' }
+  const searchParams = new URLSearchParams(queryParams)
+  const urlToFetchRecords = `${getRecordsApiEndpoint}?${searchParams}`
+  const response = await fetch(urlToFetchRecords)
+  const records = await response.json()
+
+  console.log(records)
+
+  const playlists = document.querySelector('#playlists')
+
+  playlists.innerHTML = ''
+  records.forEach((record, i) => {
+    const playlistName = record.fields['Name']
+    const playlistDescription = record.fields['Notes']
+    const selected = i === 0
+
+    if (selected) {
+      document.querySelector('#current-playlist').innerHTML = playlistName
+    }
+
+    const html = `
+      <div class="playlist ${selected ? 'playlist--selected' : ''}">
+        <span class="playlist__name">
+          <span>🎶</span> ${playlistName}
+        </span>
+        <span class="playlist__description">
+          ${playlistDescription}
+        </span>
+      </div>
+    `
+
+    playlists.innerHTML += html
+  })
+})()
+
