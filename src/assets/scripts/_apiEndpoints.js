824ef450d60356461e5580b6b485150775bcf691
kwaziphi <60526487+kwaziphi@users.noreply.github.com>
2023-09-07 19:45:25 +0800

    Update _apiEndpoints.js
    change to signed url function

--- src/assets/scripts/_apiEndpoints.js
+++ src/assets/scripts/_apiEndpoints.js
@@ -1,6 +1,7 @@
 const basePath = 'https://europe-central2-sphere-385104.cloudfunctions.net'
 
 export const loginApiEndpoint = `${basePath}/login`
-export const getRecordsApiEndpoint = `${basePath}/getRecords`
+//export const getRecordsApiEndpoint = `${basePath}/getRecords`
+export const getRecordsApiEndpoint = `${basePath}/getRecords-signedURL`
 export const updateRecordApiEndpoint = `${basePath}/updateRecordStatus`
 
