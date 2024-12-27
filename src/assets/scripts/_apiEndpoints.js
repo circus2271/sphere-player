f03273ac60158cd81049defef0a55044522b5777
circus2271 <bb@example.com>
2024-12-27 08:49:09 +0300

    fds

--- src/assets/scripts/_apiEndpoints.js
+++ src/assets/scripts/_apiEndpoints.js
@@ -1,23 +1,18 @@
 let basePath = 'https://europe-central2-sphere-385104.cloudfunctions.net'
 
-// export const changeBasePath = (url) = basePath = url
-// it may happend that google url will be banned, so in this case use cloudflare proxy
-// export const enableProxy = () => basePath = 'proxy path ...'
+// it may happen that google url will be banned, so in this case use cloudflare proxy
 export const enableProxy =  async () => {
   return new Promise(resolve => {
+    // this hack is used to make sure the link is updated
     setTimeout(() => {
       basePath = 'https://my-first-worker.hi-c5b.workers.dev'
       resolve()
     }, 0)
   })
 }
-  // basePath = 'https://my-first-worker.hi-c5b.workers.dev'
 
 export const loginApiEndpoint = () => `${basePath}/login`
-// export const getRecordsApiEndpoint = `${basePath}/getRecords`
 export const getRecordsApiEndpoint = () => `${basePath}/getRecordsFromCdn` // info + tracks
-// export const getCdnRecordsApiEndpoint = `${basePath}/getRecordsFromCdn`
-// export const getRecordsApiEndpoint = `${basePath}/getRecords-signedURL`
 export const updateRecordApiEndpoint = () => `${basePath}/updateRecordStatus`
 export const updateSongStatsApiEndpoint = () => `${basePath}/updateSongStats`
 
