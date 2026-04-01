105bcaae37a04c1b23a118f23dc8240fe596ae67
circus2271 <rodionov134@gmail.com>
2026-04-01 16:15:07 +0300

    add device unique id

--- src/assets/scripts/_player.js
+++ src/assets/scripts/_player.js
@@ -161,9 +161,20 @@ export class Player {
       stats.skipped = skipped
       stats.playlistName = this.currentPlaylistTableName
       stats.timestamp = new Date().toLocaleString('ru-RU')
+      
+      let deviceUniqueId = localStorage.getItem('deviceUniqueId')
+      if (!deviceUniqueId) {
+        const time = new Date().getTime().toString()
+        const id = time.slice(time.length-5)
+        localStorage.setItem('deviceUniqueId', id)
+
+        deviceUniqueId = id
+      }
+      stats.deviceUniqueId = deviceUniqueId
 
       setTimeout(() => {
         sendSongStats(stats)
+        // debugger
         // wait 3 seconds for hopefully pass airtable 5-requeste-at-once limit
       }, 3000)
 
